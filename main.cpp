68b9a7eabdd73f14b62dcb61b352ad0239579e43
hannanyusop <nan_s96@yahoo.com>
2018-12-15 00:41:01 +0800

    menu selection

--- main.cpp
+++ main.cpp
@@ -7,20 +7,23 @@ using namespace std;
 
 void logo()
 {
-    cout << "======================================\n";
-    cout << "|| UTHM SUBJECT REGISTRATION SYSTEM ||\n";
-    cout << "======================================\n\n";
+    cout << "==============================================================\n";
+    cout << "||              UNIVERSITI TEKNOLOGI MALAYSIA               ||\n";
+    cout << "||                      SKUDAI,JOHOR                        ||\n";
+    cout << "||          UTM SUBJECT REGISTRATION SYSTEM                 ||\n";
+    cout << "==============================================================\n";
 }
 bool login()
 {
     string username,password;
-	cout<< "\n------------------------------------------------\n";
+    
+	cout << "==============================================================\n";
     cout<< "\n\t\tPlease Login\n\n";
     cout<< "\tStudent No:";
     cin >> username;
     cout<< "\tPassword: ";
     cin >> password;
-    cout<< "\n------------------------------------------------\n";
+    cout << "==============================================================\n";
     
     bool auth = false;
     string parameter = username+"=>"+password;
@@ -49,6 +52,39 @@ bool login()
 
 }
 
+int displayMenu()
+{
+	cout << "\n############################################################\n";
+	cout << "#                       WELOCOME TO UTM CMS                #\n";
+	cout << "#                use number(1-6) to make selection         #\n";
+	cout << "############################################################\n";
+	cout << "| 1 => Add Subject                                         |\n";
+	cout << "| 2 => Delete Subject                                      |\n";
+	cout << "| 3 => Submit Subject                                      |\n";
+	cout << "| 4 => View Reqistered Subject                             |\n";
+	cout << "| 5 => View All Subject                                    |\n";
+	cout << "| 6 => Search Subject ID                                   |\n";
+	cout << "|==========================================================|\n";
+	cout << "| Q => Quit                                                |\n";
+	cout << "|==========================================================|\n";
+	
+	bool result = false; int num;
+	do{
+		cout<< "Select Menu(1-6):";
+		cin >> num;
+		
+		if(num > 0 && num < 7){
+			
+			result = true;
+		}else{
+			cout << "invalid input! Please insert range 1-6.\n\n";
+		}
+		
+	}while(result == false);
+	
+	return num;
+	
+}
 int main() {
    logo();
    bool loginResult;
@@ -69,10 +105,12 @@ int main() {
 
    }while(loginResult == 0);
 
-    std::fstream fs;
-    fs.open ("data/students/test.txt", std::fstream::in | std::fstream::out | std::fstream::app);
-    fs << " more lorem ipsum";
-    fs.close();
+    //logged in
+    system("CLS");
+    logo();
+    int selected = displayMenu();
+    cout << selected;
+    
 
    return 0;
 }
